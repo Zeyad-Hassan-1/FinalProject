463b9f220272b3e7d66e0d1767c846dbf7f2ce3b
Zeyad Hasan <studying.zezo@gmail.com>
2024-12-14 00:15:55 +0200

    UpdatedbyZeyad

--- main.c
+++ main.c
@@ -8,5 +8,6 @@ int main()
     FILE *fptr;
     RoomReservation(fptr, "Reservation.txt", "a", 0 ,"Zeyad Hasan", "535585855", "Testmail@gmail.com", "01094933404");
     printf("world");
+    printf("This made by zeyad");
     return 0;
 }
