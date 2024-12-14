4fad86e4f8ccea215d30ddce971ec872fd41901c
Zeyad Hasan <studying.zezo@gmail.com>
2024-12-14 02:45:23 +0200

    UpdatedByZeyad

--- main.c
+++ main.c
@@ -1,13 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <curses.h>
 #include "reservations.h"
 
 int main()
 {
     FILE *fptr;
     RoomReservation(fptr, "Reservation.txt", "a", 0 ,"Zeyad Hasan", "535585855", "Testmail@gmail.com", "01094933404");
-    printf("world");
-    printf("This made by zeyad");
     return 0;
 }
